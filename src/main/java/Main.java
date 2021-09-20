605a85b1b37279be9db211678d4f88630daee0f8
arsentiy82 <arssentiyy@yandex.ru>
2021-09-20 18:57:04 +0300

    20092021

---
+++ src/main/java/Main.java
@@ -0,0 +1,11 @@
+import MyTest.TestHandler;
+
+public class Main {
+    public static void main(String[] args) {
+
+        ClassForTesting classForTesting = new ClassForTesting();
+        TestHandler.start(classForTesting.getClass());
+        System.out.println();
+        ClassExplorer.outClassInfo(String.class);
+    }
+}
---
+++ src/main/java/MyTest/Test.java
@@ -0,0 +1,9 @@
+package MyTest;
+
+import java.lang.annotation.*;
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+
+public @interface  Test {
+    int priority() default 1;
+}
